98889c6b48d8abd775bd8da8437efa42628b0b33
felix <felix@bjorelid.se>
2022-12-11 16:13:48 +0100

    3

--- src/Item.js
+++ src/Item.js
@@ -4,8 +4,8 @@ export default function Item(props) {
   return (
     <li className="">
         {props.item.title ? props.item.title : '–'}
-        <div className="button-group">
-          <button className="remove" onClick={() => {props.deleteItem(props.item.id)}} >❌</button>
+        <div className="button-group"> 
+          <button className="remove"  onClick={() => {props.deleteItem(props.item.id)}} >❌</button>
           <button className="edit" onClick={() => {props.editItem(props.item.id)}} >🖊️</button>
         </div>
     </li>
